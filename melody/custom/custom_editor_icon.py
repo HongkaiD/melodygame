fe4463175ea65939649d03ee3bb44d730a55ffeb
HongkaiD <jamesding22d@gmail.com>
2023-08-15 20:17:05 +0800

    提交1

---
+++ melody/custom/custom_editor_icon.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {134258961: {'path': 'ui/134258961', 'describe': '', 'name': u'menu', 'key': 134258961, 'uid': '134258961', 'size': '1920X1080'}}
