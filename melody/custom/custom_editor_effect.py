69b96a1107d393429263e76ea015af77bb5ec71d
HongkaiD <75577452+HongkaiD@users.noreply.github.com>
2023-08-18 02:27:04 +0800

    更新场景1

--- melody/custom/custom_editor_effect.py
+++ melody/custom/custom_editor_effect.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134218867: {'model_id': 134241038, 'preview_icon': '', 'name': u'\u7bad\u5934', 'describe': 134218867, 'key': 134218867, 'res_identify': '', 'scale': 1.0, 'uid': '134218867', 'source_effect': 102455}, 134260616: {'model_id': 134241380, 'preview_icon': '', 'name': u'\u9646\u5730', 'describe': 134260616, 'key': 134260616, 'res_identify': '', 'scale': 1.0, 'uid': '134260616', 'source_effect': 102455}}
--- melody/custom/custom_model.py
+++ melody/custom/custom_model.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {}
+data = {134241038: {'name': u'\u7bad\u5934', 'model_id': 134241038, 'effect_path': 'Fx_Effect/OtherType/Other_Type_blank_copy', 'meshes': (), 'custom': True, 'effect_keep': True, 'effect_max_life': -1}, 134241380: {'name': u'\u9646\u5730', 'model_id': 134241380, 'effect_path': 'Fx_Effect/OtherType/Other_Type_blank_copy_1', 'meshes': (), 'custom': True, 'effect_keep': True, 'effect_max_life': -1}}
