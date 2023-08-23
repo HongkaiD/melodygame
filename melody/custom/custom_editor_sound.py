98a9259821b3b7bfb680228536fba2612e3f3a18
HongkaiD <jamesding22d@gmail.com>
2023-08-23 22:19:09 +0800

    更新

--- melody/custom/custom_editor_sound.py
+++ melody/custom/custom_editor_sound.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134270488: {'SoundPath': '134270488', 'describe': '', 'name': u'263133__pan14__tone-beep', 'key': 134270488, 'Format': u'wav', 'uid': 134270488, 'IsCustom': True, 'source': 'code_Source_Import'}, 134278539: {'SoundPath': '134278539', 'describe': '', 'name': u'metal-dark-matter-111451', 'key': 134278539, 'Format': u'mp3', 'uid': 134278539, 'IsCustom': True, 'source': 'code_Source_Import'}, 134263724: {'SoundPath': '134263724', 'describe': '', 'name': u'Ghostface_Playa_fkbambam_-_KILLKA', 'key': 134263724, 'Format': u'mp3', 'uid': 134263724, 'IsCustom': True, 'source': 'code_Source_Import'}, 134240632: {'SoundPath': '134240632', 'describe': '', 'name': u'zamona-net-dxrk-rave', 'key': 134240632, 'Format': u'mp3', 'uid': 134240632, 'IsCustom': True, 'source': 'code_Source_Import'}, 134279225: {'SoundPath': '134279225', 'describe': '', 'name': u'rock-the-party-110947', 'key': 134279225, 'Format': u'mp3', 'uid': 134279225, 'IsCustom': True, 'source': 'code_Source_Import'}}
